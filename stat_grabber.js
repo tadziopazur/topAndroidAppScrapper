b1e5b5863b3eedd9de54cc83d75a346cb92aa6cd
tadziopazur <wozniak.tt@gmail.com>
2018-07-03 13:54:44 +0200

    README added, partial scrapper added

--- stat_grabber.js
+++ stat_grabber.js
@@ -1,4 +1,6 @@
-var https = require('https');
+var https = require('https'),
+    jsdom = require('jsdom').JSDOM;
+
 var startPage = "https://www.androidrank.org/listcategory?category=&sort=0&hl=en";
 //start=1, 21, 41, ...
 //price=free | paid
@@ -11,11 +13,37 @@ var requestOptions = {
   method: 'GET'
 };
 
+function scrapper(pageData) {
+  console.log('Data is ', pageData);
+  var document = (new jsdom(pageData)).window.document;
+  console.log('Document is ', document);
+  var    ranklist = document.getElementById('ranklist');
+  console.log('ranklist is ', ranklist);
+  var elementList = ranklist.querySelectorAll('tr.odd, tr.even');
+
+  elementList.foreach((item, index) => {
+    var childNodes = item.childNodes(),
+      noNode = childNodes[0],
+      nameNode = childNodes[1],
+      ratingNode = childNodes[3],
+      installNode = childNodes[4],
+      ratingNode = childNodes[5],
+      priceNode = childNodes[8];
+
+    console.log('> ', noNode.innerHTML, nameNode.innerHTML, ratingNode.innerHTML, 
+                installNode.innerHTML, ratingNode.innerHTML, priceNode.innerHTML);
+  });
+}
+
+//function responseHandler(pageDataHandler, response) {
 function responseHandler(response) {
   console.log("Response: ", response.statusCode);
   console.log("Resp headers: ", response.headers);
+
+  var dataBuffer = "";
   try {
       req.on('data', (data) => {
+        dataBuffer += data;
         console.log("" + data.length + " bytes of data\n");
       });
   } catch (e) {
@@ -24,7 +52,9 @@ function responseHandler(response) {
   req.on('error', (error) => {
     console.log('Rquest error: ', error);
   });
+  scrapper(dataBuffer);
   req.end();
+  //pageDataHandler(dataBuffer);
 }
 
 var path = '/listcategory?category=&sort=0&hl=en';
@@ -33,11 +63,10 @@ for (start = 1; start < 20; start += 20) {
   requestOptions.path = path + "&start=" + start + "&price=" + price;
   console.log("Requesting " + requestOptions.protocol + requestOptions.hostname + requestOptions.path);
   try {
-    var req = https.request(requestOptions, responseHandler);
+    var req = https.request(requestOptions, responseHandler);//.bind(null, scrapper));
     console.log("Request made");
   } catch (e) {
     console.log("Request error: " + e.message);
   }
   req.end();
 }
-
