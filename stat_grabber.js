9f7958c948af571a26543372ba398ad0a826f9ef
tadziopazur <wozniak.tt@gmail.com>
2018-07-10 01:38:26 +0200

    Scrappers level 1 works, feed from files

--- stat_grabber.js
+++ stat_grabber.js
@@ -1,42 +1,44 @@
 var https = require('https'),
-    jsdom = require('jsdom').JSDOM;
-
-var startPage = "https://www.androidrank.org/listcategory?category=&sort=0&hl=en";
-//start=1, 21, 41, ...
-//price=free | paid
-
-var requestOptions = {
-  protocol: 'https:',
-  hostname: 'www.androidrank.org',
-  path: '/',
-  port: 443,
-  method: 'GET'
-};
+    jsdom = require('jsdom').JSDOM,
+    fs = require('fs');
 
 function scrapper(pageData) {
-  console.log('Data is ', pageData);
+  //console.log('Data is ', pageData);
   var document = (new jsdom(pageData)).window.document;
   console.log('Document is ', document);
   var    ranklist = document.getElementById('ranklist');
   console.log('ranklist is ', ranklist);
   var elementList = ranklist.querySelectorAll('tr.odd, tr.even');
 
-  elementList.foreach((item, index) => {
-    var childNodes = item.childNodes(),
-      noNode = childNodes[0],
-      nameNode = childNodes[1],
-      ratingNode = childNodes[3],
-      installNode = childNodes[4],
-      ratingNode = childNodes[5],
-      priceNode = childNodes[8];
+  //console.log("elementList is a ", typeof(elementList.forEach));
+  var first = true;
+  elementList.forEach((item, index) => {
+    if (first) {
+      console.log('item is of type ', item, ', at index ', index);
+      console.log('item is ', item.innerHTML);
+      first = false;
+      var childNodes = item.childNodes,
+        noNode = childNodes[1],
+        nameNode = childNodes[2],
+        linkNode = childNodes[3].childNodes[1],
+        imageNode = childNodes[5],
+        ratingCountNode = childNodes[7],
+        installNode = childNodes[9],
+        ratingNode = childNodes[11].childNodes[0],
+        priceNode = childNodes[17].childNodes[0];
+//      console.log('Name node is of type ', typeof(ratingNode), ' -> ', ratingNode, ', children ', ratingNode.childNodes);
 
-    console.log('> ', noNode.innerHTML, nameNode.innerHTML, ratingNode.innerHTML, 
-                installNode.innerHTML, ratingNode.innerHTML, priceNode.innerHTML);
+      console.log('> ', noNode.innerHTML, '|', 
+                        linkNode.innerHTML, '->', linkNode.href, '|', 
+                        ratingCountNode.innerHTML, '|', 
+                        installNode.innerHTML, '|',
+                        ratingNode.innerHTML, '|',
+                        priceNode.innerHTML);
+    }
   });
 }
 
-//function responseHandler(pageDataHandler, response) {
-function responseHandler(response) {
+function responseHandler(pageDataHandler, response) {
   console.log("Response: ", response.statusCode);
   console.log("Resp headers: ", response.headers);
 
@@ -44,7 +46,7 @@ function responseHandler(response) {
   try {
       req.on('data', (data) => {
         dataBuffer += data;
-        console.log("" + data.length + " bytes of data\n");
+        console.log(data.length, " bytes of data\n");
       });
   } catch (e) {
     console.log("Response error: " + e.message);
@@ -54,19 +56,45 @@ function responseHandler(response) {
   });
   scrapper(dataBuffer);
   req.end();
-  //pageDataHandler(dataBuffer);
+  pageDataHandler(dataBuffer);
+}
+
+// Pushes the contents of the desired page into the callback
+function getPage(price, start, live, callback) {
+  if (live) {
+    var startPage = "https://www.androidrank.org/listcategory?category=&sort=0&hl=en";
+    var requestOptions = {
+      protocol: 'https:',
+      hostname: 'www.androidrank.org',
+      path: '/',
+      port: 443,
+      method: 'GET'
+    };
+    requestOptions.path = path + "&start=" + start + "&price=" + price;
+    console.log("Requesting " + requestOptions.protocol + requestOptions.hostname + requestOptions.path);
+    try {
+      var req = https.request(requestOptions, responseHandler.bind(null, callback));
+    } catch (e) {
+      console.log("Request error: " + e.message);
+    }
+    req.end();
+  } else {
+    var fileName = "files/app_stats_" + price + "_" + start;
+    fs.readFile(fileName,
+      (err, data) => {
+        if (err) {
+          console.error("Cannot read file ", fileName);
+          throw(err);
+        }
+        console.log(data.length, "of file data read from ", fileName);
+        callback(data);
+      });
+  }
 }
 
 var path = '/listcategory?category=&sort=0&hl=en';
 var price= "free";
 for (start = 1; start < 20; start += 20) {
-  requestOptions.path = path + "&start=" + start + "&price=" + price;
-  console.log("Requesting " + requestOptions.protocol + requestOptions.hostname + requestOptions.path);
-  try {
-    var req = https.request(requestOptions, responseHandler);//.bind(null, scrapper));
-    console.log("Request made");
-  } catch (e) {
-    console.log("Request error: " + e.message);
-  }
-  req.end();
+  getPage(price, start, false, scrapper);
 }
+
