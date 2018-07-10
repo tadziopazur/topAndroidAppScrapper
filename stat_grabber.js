66dc2c2c4eea0c62ca3c94a9cadb951d4fb78991
tadziopazur <wozniak.tt@gmail.com>
2018-07-10 02:06:29 +0200

    WiP with some dead code. Deep scrapper not working

--- stat_grabber.js
+++ stat_grabber.js
@@ -2,21 +2,21 @@ var https = require('https'),
     jsdom = require('jsdom').JSDOM,
     fs = require('fs');
 
-function scrapper(pageData) {
+function scrapper(url, pageData) {
   //console.log('Data is ', pageData);
   var document = (new jsdom(pageData)).window.document;
-  console.log('Document is ', document);
+  //console.log('Document is ', document);
   var    ranklist = document.getElementById('ranklist');
-  console.log('ranklist is ', ranklist);
+  //console.log('ranklist is ', ranklist);
   var elementList = ranklist.querySelectorAll('tr.odd, tr.even');
 
   //console.log("elementList is a ", typeof(elementList.forEach));
   var first = true;
   elementList.forEach((item, index) => {
     if (first) {
-      console.log('item is of type ', item, ', at index ', index);
-      console.log('item is ', item.innerHTML);
-      first = false;
+      //console.log('item is of type ', item, ', at index ', index);
+      //console.log('item is ', item.innerHTML);
+      //first = false;
       var childNodes = item.childNodes,
         noNode = childNodes[1],
         nameNode = childNodes[2],
@@ -26,9 +26,17 @@ function scrapper(pageData) {
         installNode = childNodes[9],
         ratingNode = childNodes[11].childNodes[0],
         priceNode = childNodes[17].childNodes[0];
+      var no = parseInt(noNode.innerHTML),
+          name = linkNode.innerHTML,
+          link = linkNode.href,
+          ratingCount = parseInt(ratingCountNode.innerHTML.replace(/,/g, "")),
+          installRange = installNode.innerHTML,
+          rating = ratingNode.innerHTML,
+          price = priceNode.innerHTML;
 //      console.log('Name node is of type ', typeof(ratingNode), ' -> ', ratingNode, ', children ', ratingNode.childNodes);
 
-      console.log('> ', noNode.innerHTML, '|', 
+      console.log('> ', no, name, link, ratingCount, installRange, rating, price);
+      if (false) console.log('> ', noNode.innerHTML, '|', 
                         linkNode.innerHTML, '->', linkNode.href, '|', 
                         ratingCountNode.innerHTML, '|', 
                         installNode.innerHTML, '|',
@@ -54,7 +62,6 @@ function responseHandler(pageDataHandler, response) {
   req.on('error', (error) => {
     console.log('Rquest error: ', error);
   });
-  scrapper(dataBuffer);
   req.end();
   pageDataHandler(dataBuffer);
 }
@@ -73,7 +80,8 @@ function getPage(price, start, live, callback) {
     requestOptions.path = path + "&start=" + start + "&price=" + price;
     console.log("Requesting " + requestOptions.protocol + requestOptions.hostname + requestOptions.path);
     try {
-      var req = https.request(requestOptions, responseHandler.bind(null, callback));
+      var req = https.request(requestOptions, 
+                              responseHandler.bind(null, callback.bind(null, requestOptions.hostname)));
     } catch (e) {
       console.log("Request error: " + e.message);
     }
@@ -87,7 +95,7 @@ function getPage(price, start, live, callback) {
           throw(err);
         }
         console.log(data.length, "of file data read from ", fileName);
-        callback(data);
+        callback(null, data);
       });
   }
 }
